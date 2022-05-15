22840b7f9fd31220f4983a326a95694eb7a7f147
Nickx58 <nikhilx58@gmail.com>
2022-05-15 22:53:34 +0530

    Star Loop Pattern

---
+++ Loop_Patterns/1.SimpleStarPattern.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	// Your code goes here;
+	for(int i =0; i<3;i++) {
+		for(int j = 0; j<3;j++) {
+			cout<<'*';
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
