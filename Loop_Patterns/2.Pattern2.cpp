2b9e7d13f06d0ebfe9e8acc9552eec8b758e65bd
Nickx58 <nikhilx58@gmail.com>
2022-05-28 13:19:17 +0530

    Star Pattern

--- Loop_Patterns/2.Pattern2.cpp
+++ Loop_Patterns/2.Pattern2.cpp
@@ -47,3 +47,23 @@ int main() {
 	}
 	return 0;
 }
+
+/*
+star pattern aka Traingle Pattern
+*
+**
+***
+****
+*/
+
+int main() {
+    int n = 4;
+	int count = 1;
+	for(int i = 1; i<=n;i++) {
+		for(int j = 1;j<=i;j++) {
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
