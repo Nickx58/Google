ad49e713d6eaea640c48f63c368339b8f6a3daee
Nickx58 <nikhilx58@gmail.com>
2022-05-28 14:37:38 +0530

    Character Pattern

---
+++ Loop_Patterns/3.CharacterPattern.cpp
@@ -0,0 +1,23 @@
+/*
+AAA
+BBB
+CCC
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n = 3;
+	for(int i = 1; i<=n;i++) {
+		for(int j = 1;j<=n;j++) {
+			char ch = 'A' + i - 1;
+			cout<<ch;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
