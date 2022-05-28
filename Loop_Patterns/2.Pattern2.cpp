ff90a47094cdcfce0ca4487a7ff6ff943c500193
Nickx58 <nikhilx58@gmail.com>
2022-05-28 13:08:22 +0530

    Loop Pattern 3

--- Loop_Patterns/2.Pattern2.cpp
+++ Loop_Patterns/2.Pattern2.cpp
@@ -22,3 +22,28 @@ int main() {
 	}
 	return 0;
 }
+
+/*
+123
+456
+789
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n = 3;
+	int count = 1;
+	for(int i = 1; i<=n;i++) {
+		for(int j = 1;j<=n;j++) {
+			cout<<count;
+			count++;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
