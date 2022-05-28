600be1f4eb70ec84b293fd7f74ab1eb53ffc69b0
Nickx58 <nikhilx58@gmail.com>
2022-05-28 14:50:22 +0530

    Character Pattern 2

--- Loop_Patterns/3.CharacterPattern.cpp
+++ Loop_Patterns/3.CharacterPattern.cpp
@@ -21,3 +21,21 @@ int main() {
 	}
 	return 0;
 }
+
+/*
+ABC
+ABC
+ABC
+*/
+
+int main() {
+    int n = 3;
+	for(int i = 1; i<=n;i++) {
+		for(int j = 1;j<=n;j++) {
+			char ch = 'A' + j - 1;
+			cout<<ch;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
