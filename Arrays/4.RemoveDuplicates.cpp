40ac7de63f327f68fcd9bf0a985672c54b8634a6
Nickx58 <nikhilx58@gmail.com>
2022-10-02 11:13:00 +0530

    Remove Duplicates from Array

---
+++ Arrays/4.RemoveDuplicates.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<int> removeDuplicates(int* a, int size) {
+    vector<int> output;
+    unordered_map<int, bool> seen;
+    for(int i = 0; i<size; i++) {
+        if(seen.count(a[i]) > 0) {
+            continue;
+        }
+        seen[a[i]] = true;
+        output.push_back(a[i]);
+    }
+    return output;
+}
+
+int main() {
+    int a[] = {1,2,2,3,3,4,4,5,6,6};
+    int size = sizeof(a)/sizeof(a[0]);
+    vector<int> output = removeDuplicates(a, size);
+    for(int i = 0; i<output.size();i++) {
+        cout<<output[i]<<endl;
+    }
+    return 0;
+}
