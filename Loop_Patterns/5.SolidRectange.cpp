2a54627362773d7a407e784499e2b49398427d68
Nickx58 <nikhilx58@gmail.com>
2022-12-29 18:42:11 +0530

    Loop: Pattern Solid Rectangle

---
+++ Loop_Patterns/5.SolidRectange.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+*****
+*****
+*****
+*****
+*/
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
