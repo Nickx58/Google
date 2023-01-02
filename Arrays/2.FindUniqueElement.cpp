979380775396ccf25f1120ccf8697cfe72ae1214
Nickx58 <nikhilx58@gmail.com>
2023-01-02 23:11:31 +0530

    Array: Find Unique Element in the array

--- Arrays/2.FindUniqueElement.cpp
+++ Arrays/2.FindUniqueElement.cpp
@@ -13,3 +13,27 @@ int main()
     cout<<answer;
 
 }
+/*
+o(n)2 complexity
+*/
+
+int main() {
+    int arr[] = {2, 2, 2, 10, 2};
+    int k = 3;
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int count;
+    for(int i = 0; i<size; i++) {
+        count = 0;
+        for(int j = 0; j<size; j++) {
+            if(i != j) {
+       if(arr[i] == arr[j]) {
+           count++;
+       }
+            }  
+        }
+        if(count == 0) {
+            cout<<arr[i]<<endl;
+        }
+    }
+    return 0;
+}
