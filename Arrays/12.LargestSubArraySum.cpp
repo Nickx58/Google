f07f3419a097595e33a506cf43bfd13e3844e0d7
Nickx58 <nikhilx58@gmail.com>
2023-02-18 11:34:53 +0530

    Array: Maximum Sum Sub-Array Prefix Sum

--- Arrays/12.LargestSubArraySum.cpp
+++ Arrays/12.LargestSubArraySum.cpp
@@ -5,7 +5,7 @@ Approach 1st is the Brute Force. Trying all the possiblities
 #include <bits/stdc++.h>
 using namespace std;
 
-void printSubArrays(int arr[], int size) {
+void largestSubArrays(int arr[], int size) {
     int largest_sum = 0;
     for(int i = 0; i<size; i++) {
         for(int j = i; j<size; j++) {
@@ -24,7 +24,43 @@ void printSubArrays(int arr[], int size) {
 int main() {
     int arr[] = {-2,3,4,-1,5,-12,6,1,3};
     int size = sizeof(arr)/sizeof(int);
-    printSubArrays(arr, size);
+    largestSubArrays(arr, size);
     return 0;
 }
+
+/*
+prefix Sum method
+
+*/
+    
+#include <bits/stdc++.h>
+using namespace std;
+
+void largestSubArray(int arr[], int size) {
+    // Prefix sum
+    int prefix[size] = {0};
+    prefix[0] = arr[0];
+    for(int i = 1; i<size; i++) {
+        prefix[i] = prefix[i-1] + arr[i];
+    }
+    
+    int largest_sum = 0;
+    for(int i = 0; i<size; i++) {
+        for(int j = i; j<size; j++) {
+            int currentSum = i>0 ? prefix[j] - prefix[i-1] : prefix[j];
+            if(currentSum > largest_sum) {
+                largest_sum = currentSum;
+            }
+        }
+    }
+    cout<<"Largest Subarray sum is: "<<largest_sum<<endl;
+}
+
+int main() {
+    int arr[] = {-2,3,4,-1,5,-12,6,1,3};
+    int size = sizeof(arr)/sizeof(int);
+    largestSubArray(arr, size);
+    return 0;
+}
+
     
