defa543ea84e0da3e9ec70ae1adae593a7118cdc
Nickx58 <nikhilx58@gmail.com>
2023-05-16 22:46:32 +0530

    Array Rotated and Sorted

---
+++ NeetCode/Arrays_Hashing/6.CheckArrayRotatedAndSorted.cpp
@@ -0,0 +1,21 @@
+/*
+https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
+*/
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        for(int i = 0; i<n-1; i++) {
+            if(nums[i] > nums[i+1]) {
+                count++;
+            }
+        }
+
+        if(nums[0] < nums[n-1]) {
+            count++;
+        }
+        return (count<=1);
+    }
+};
