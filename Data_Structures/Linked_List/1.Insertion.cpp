6c10d1b31f287a8cb751360d8cac4c917e425bdf
Nickx58 <nikhilx58@gmail.com>
2023-08-08 18:30:11 +0530

    Linked List: Insert at Given Position

--- Data_Structures/Linked_List/1.Insertion.cpp
+++ Data_Structures/Linked_List/1.Insertion.cpp
@@ -39,14 +39,30 @@ void append(Node* &head, int data) {
 
     last->next = newNode;
 }
+
+void insertAtPosition(Node* &head, int data, int position) {
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->next = NULL;
+
+    // traverse to the n-1 position;
+    Node* temp = head;
+    for(int i = 1; i<position-1; i++) {
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
 int main() {
     Node* head = NULL;
     push(head, 10);
     push(head, 100);
     push(head, 1000);
-    print(head);
-    cout<<"Append the last"<<endl;
     append(head, 5);
+    print(head);
+    insertAtPosition(head, 44,3);
+    print(head);
+    insertAtPosition(head, 444,4);
     print(head);
     return 0;
 }
