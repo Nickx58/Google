5d104a2308df4ab5174f4ec6b3bccc660be04e86
Nickx58 <nikhilx58@gmail.com>
2023-08-13 21:59:59 +0530

    Javascript: Debounce

--- Javascript/Polyfills/map.js
+++
@@ -1,7 +0,0 @@
-Array.prototype.myMap = function(callback) {
-    const output = [];
-    for(let i = 0; i<this.length; i++) {
-        output.push(callback(this[i],i,this)); // this[i] -> current element, i -> index, this -> array;
-    }
-    return output;
-}
---
+++ javascript/Polyfill/debounce.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function} func
+ * @param {number} wait
+ * @return {Function}
+ */
+export default function debounce(func, wait) {
+    let timeoutId = null;
+    return function(...args) {
+      const context = this;
+      clearTimeout(timeoutId);
+  
+      timeoutId = setTimeout(function() {
+        func.apply(context,args);
+      }, wait);
+    };
+  }
