90030e4f71911d382ba7871a127b0bf1ae564f38
Nickx58 <nikhilx58@gmail.com>
2023-08-15 18:31:26 +0530

    Javascript: Promise.race

---
+++ javascript/Promises/promise.Race.js
@@ -0,0 +1,14 @@
+function promiseRace(promises) {
+    return new Promise(function(resolve,reject) {
+        promises.forEach(function(promise) {
+            Promise.resolve(promise).then(resolve,reject)
+        })
+    })
+}
+
+const racePromise1 = new Promise((resolve) => setTimeout(resolve, 2000, 'one'));
+const racePromise2 = new Promise((resolve) => setTimeout(resolve, 1000, 'two'));
+
+promiseRace([racePromise1, racePromise2]).then((value) => {
+  console.log('Promise.race Result:', value); // Output: 'two'
+});
