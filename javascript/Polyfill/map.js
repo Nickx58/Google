df72e31b0b74c475b52c12f1165c1c4bb6bcd7e7
Nickx58 <nikhilx58@gmail.com>
2023-08-15 15:42:57 +0530

    Javascript: Map

--- javascript/Polyfill/map.js
+++ javascript/Polyfill/map.js
@@ -1,12 +1,11 @@
-if(!Array.prototype.reduce) {
-    Array.prototype.reduce = function(callback, initialValue) {
-        const arr = this;
-        const length = arr.length;
-        let accumulator = initialValue === undefined ? arr[0] : initialValue;
-
-        for(let i = initialValue === undefined ? 1 : 0 ; i<length; i++) {
-            accumulator = callback(accumulator, arr[i], i, arr);
-        }
-        return accumulator;
+if (!Array.prototype.map) {
+  Array.prototype.map = function (callback, thisArgs) {
+    const arr = this;
+    const length = arr.length;
+    const result = new Array(length);
+    for (let i = 0; i < length; i++) {
+      result[i] = callback.call(thisArgs, arr[i], i, arr);
     }
+    return result;
+  };
 }
