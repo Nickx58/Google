875c0e5ea12af09984d4c02089bc4c3ce4703b6c
Nickx58 <nikhilx58@gmail.com>
2023-08-27 10:58:35 +0530

    Array-Basic: Largest Element in Array

---
+++ Data_Structures/Arrays/Basic/LargestElementInArray.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n) {
+    // Write your code here.
+    int largest = INT_MIN;
+
+    for(int i = 0; i<arr.size(); i++) {
+        if(arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
