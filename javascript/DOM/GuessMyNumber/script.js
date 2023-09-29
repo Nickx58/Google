ac94780878073cb88dba40ca9582418636a8409c
Nickx58 <nikhilx58@gmail.com>
2023-09-29 13:07:17 +0530

    Javascript: Handling Event

--- javascript/DOM/GuessMyNumber/script.js
+++ javascript/DOM/GuessMyNumber/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function() {
+    const inputValue = Number(document.querySelector('.guess').value);
+    if(!inputValue) {
+        document.querySelector('.message').textContent = 'No Number'
+        return;
+    }
+})
