7710948c599597a494bb4f96a05cb9d5fc39efd5
Nickx58 <nikhilx58@gmail.com>
2023-09-29 23:41:32 +0530

    Javascript: Implement onClick and DOM operation

--- javascript/DOM/GuessMyNumber/script.js
+++ javascript/DOM/GuessMyNumber/script.js
@@ -1,9 +1,27 @@
-'use strict';
+"use strict";
 
-document.querySelector('.check').addEventListener('click', function() {
-    const inputValue = Number(document.querySelector('.guess').value);
-    if(!inputValue) {
-        document.querySelector('.message').textContent = 'No Number'
-        return;
-    }
-})
+/*
+State Variables
+*/
+const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".number").textContent = number;
+document.querySelector(".check").addEventListener("click", function () {
+  const guessNumber = Number(document.querySelector(".guess").value);
+  if (!guessNumber) {
+    document.querySelector(".message").textContent = "No Number";
+    return;
+  }
+  if (score == 1) {
+    document.querySelector(".message").textContent = "You lost the game.";
+  } else if (number === guessNumber) {
+    document.querySelector(".message").textContent = "Correct Number";
+  } else if (number < guessNumber) {
+    document.querySelector(".message").textContent = "Too High";
+    score--;
+  } else if (number > guessNumber) {
+    document.querySelector(".message").textContent = "Too Low";
+    score--;
+  }
+  document.querySelector(".score").textContent = score;
+});
