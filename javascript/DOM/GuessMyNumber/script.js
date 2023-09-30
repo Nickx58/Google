696cb75c5a8f573b3ee3e1fd0804c4b5154fc959
Nickx58 <nikhilx58@gmail.com>
2023-09-30 11:33:25 +0530

    Javascript: Implement Game Logic

--- javascript/DOM/GuessMyNumber/script.js
+++ javascript/DOM/GuessMyNumber/script.js
@@ -3,9 +3,10 @@
 /*
 State Variables
 */
-const number = Math.trunc(Math.random() * 20) + 1;
+let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = number;
+let highscore = 0;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guessNumber = Number(document.querySelector(".guess").value);
   if (!guessNumber) {
@@ -15,9 +16,14 @@ document.querySelector(".check").addEventListener("click", function () {
   if (score == 0) {
     document.querySelector(".message").textContent = "You lost the game.";
   } else if (number === guessNumber) {
+    document.querySelector(".number").textContent = number;
     document.querySelector(".message").textContent = "Correct Number";
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   } else if (number < guessNumber) {
     document.querySelector(".message").textContent = "Too High";
     score--;
@@ -26,4 +32,15 @@ document.querySelector(".check").addEventListener("click", function () {
     score--;
   }
   document.querySelector(".score").textContent = score;
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  number = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".score").textContent = 20;
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
