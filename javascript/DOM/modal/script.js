fbc3fb11b0944cdb66fb9d7650f5c444680d0e4a
Nickx58 <nikhilx58@gmail.com>
2023-10-02 17:50:39 +0530

    Javascript: Modal Code

--- javascript/DOM/modal/script.js
+++ javascript/DOM/modal/script.js
@@ -1,1 +1,29 @@
-'use strict'
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeButton = document.querySelector(".close-modal");
+const openButtonModal = document.querySelectorAll(".show-modal");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const addModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < openButtonModal.length; i++) {
+  openButtonModal[i].addEventListener("click", addModal);
+}
+
+closeButton.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
