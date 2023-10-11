0bc4e3a82f499812cc81c0f0c6b3829199b399a0
Nickx58 <nikhilx58@gmail.com>
2023-10-11 12:10:27 +0530

    CP: 1791A

---
+++ CP/CodeForces/1791A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    while(n--) {
+        char c;
+        cin>>c;
+        if(c == 'c' || c== 'o' || c == 'd' || c== 'e' || c=='f' || c=='r' || c=='s') {
+            cout<<"YES"<<endl;
+        } else {
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
