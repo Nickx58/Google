ea2b43aae6251e7a760fee0931181f3b15cc0f19
Nickx58 <nikhilx58@gmail.com>
2023-10-11 11:35:38 +0530

    CP: 266A Fixed

--- CP/CodeForces/266A.cpp
+++ CP/CodeForces/266A.cpp
@@ -2,16 +2,15 @@
 #include <string>
 
 using namespace std;
-
-
 int main() {
     // Write C++ code here
+    int n;
+    cin>>n;
     string s;
     cin>>s;
-
     int count = 0;
 
-    for(int i =0 ; i<s.size()-1; i++) {
+    for(int i = 0; i<s.size() - 1; i++) {
         if(s[i] == s[i+1]) {
             count++;
         }
