f756d041dee74970bfa3e05f25138825af0ede44
Nickx58 <nikhilx58@gmail.com>
2023-10-11 20:03:56 +0530

    Resursion: Print number in increasing order

---
+++ Data_Structures/Recursion/PrintNumberIncreasingOrder.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> increasingNumbers(int N) {
+    if(N == 1) {
+        return { 1 }; 
+    }
+    
+    vector<int>result = increasingNumbers(N-1);
+    result.push_back(N);
+    return result;
+}
