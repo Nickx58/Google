c1a329148df57a0bcc6d783913359b5b55dc9495
Nickx58 <nikhilx58@gmail.com>
2023-11-06 19:48:20 +0530

    STL: Unordered_Map c++

--- STL/unordered_map.cpp
+++ STL/unordered_map.cpp
@@ -1,29 +1,51 @@
-#include <iostream>
-#include <unordered_map>
-#include <string>
+#include<bits/stdc++.h>
+
 using namespace std;
 
 int main() {
-    unordered_map<string, int> ourmap;
+    unordered_map<string, int> myMap;
 
-    // insert
-    ourmap["nikhil"] = 27;
-    ourmap["nisha"] = 28;
+    myMap["nikhil"] = 28;
+    myMap["Nisha"] = 29;
 
-    // find or access
-    cout<<ourmap["nikhil"]<<endl;
-    cout<<ourmap.at("nikhil")<<endl;
+    // One Way
+    for(auto x: myMap) {
+        cout<<x.first<<" "<<x.second<<endl;
+    }
+    cout<<endl;
 
-    // what happen when we try to access or find the key that is not present
-    cout<<ourmap["as"]<<endl; // insert that key and assign value 0 to it
-    // cout<<ourmap.at("as")<<endl; // through expections
+    // second way
+    for(auto itr = myMap.begin(); itr != myMap.end(); itr++) {
+        cout<<itr->first<<" "<<itr->second<<endl;
+    }
+    cout<<endl<<"***********"<<endl;
+    string key = "nikhil";
 
-    // check presense
-    if(ourmap.count("as") > 0) {
-        cout<<"as is present"<<endl;
+    if(myMap.find(key) != myMap.end()) {
+       auto temp = myMap.find(key);
+       cout<<"Key is "<<temp->first<<endl;
+       cout<<"Value is "<<temp->second<<endl;
+    }
+    cout<<endl;
+    // one way
+    myMap.insert({"aditi", 12});
+
+    // another way
+    myMap.insert(make_pair("mobile", 5200));
+    for(auto itr = myMap.begin(); itr != myMap.end(); itr++) {
+        cout<<itr->first<<" "<<itr->second<<endl;
     }
 
-    // erase
-    ourmap.erase("as");
-    return 0;
+    unordered_map<int,int> newMap;
+
+    vector<int>v = {1,2,2,3,4,5,5,6,7,7,8,9,9};
+
+    // mark the frequency of each element
+    for(int i = 0; i<v.size(); i++) {
+        int key = v[i];
+        newMap[key]++;
+    }
+    for(auto itr = newMap.begin(); itr != newMap.end(); itr++) {
+        cout<<itr->first<<" "<<itr->second<<endl;
+    }
 }
