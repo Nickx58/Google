a9205b87415a089b723a76e5a34cb098b77d5ddb
Nickx58 <nikhilx58@gmail.com>
2023-11-11 11:54:56 +0530

    Added Button and Prop-Type Lib

--- react/react-comps/src/App.js
+++ react/react-comps/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Button from "./components/Button";
 
 function App() {
 
   return (
     <div className="app">
       <h1>React Comp</h1>
+      <Button>Click ME</Button>
     </div>
   );
 }
