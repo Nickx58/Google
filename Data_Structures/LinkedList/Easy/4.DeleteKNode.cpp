953270e08ca5aa89ba783e85069fded0ec7ab896
Nickx58 <nikhilx58@gmail.com>
2023-12-03 12:44:46 +0530

    Linked List: Delete Linked List with the Value Node

--- Data_Structures/LinkedList/Easy/4.DeleteKNode.cpp
+++ Data_Structures/LinkedList/Easy/4.DeleteKNode.cpp
@@ -1,57 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node {
-    public: // access modifier
-    
-    int data; // data value
-    Node* next; // pointer to next node
+class Node
+{
+public:         // access modifier
+    int data;   // data value
+    Node *next; // pointer to next node
 
-    public: // constructor
-    Node(int data1) {
-        data = data1; // initialize the data with next value;
+public: // constructor
+    Node(int data1)
+    {
+        data = data1;   // initialize the data with next value;
         next = nullptr; // initialize the next as null
     }
 };
 
-Node* convertArrayToLL(vector<int> &arr) {
-    Node* head = new Node(arr[0]);
-    Node* mover = head;
-    for(int i = 1; i<arr.size(); i++) {
-        Node* temp = new Node(arr[i]);
+Node *convertArrayToLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
         mover->next = temp;
         mover = temp;
     }
     return head;
 }
 
-void printList(Node* head) {
-    Node* temp = head;
-    while(temp) {
-        cout<<temp->data<<" ";
+void printList(Node *head)
+{
+    Node *temp = head;
+    while (temp)
+    {
+        cout << temp->data << " ";
         temp = temp->next;
     }
 }
 
-
-Node* deleteK(Node* head, int k) {
-    if(head == NULL) {
+Node *deleteK(Node *head, int k)
+{
+    if (head == NULL)
+    {
         return NULL;
     }
 
-    if(k == 1) {
-        Node* temp = head;
+    if (k == 1)
+    {
+        Node *temp = head;
         head = head->next;
         delete temp;
         return head;
     }
-    Node* temp = head;
+    Node *temp = head;
     int count = 0;
-    Node* prev = NULL;
+    Node *prev = NULL;
 
-    while(temp) {
+    while (temp)
+    {
         count++;
-        if(count == k) {
+        if (count == k)
+        {
             prev->next = prev->next->next;
             delete temp;
             break;
@@ -62,12 +71,44 @@ Node* deleteK(Node* head, int k) {
     return head;
 }
 
-int main() {
-    vector<int> arr = {1,2,3,4,5,6};
-    Node* head = convertArrayToLL(arr);
+Node *deleteValueNode(Node *head, int val)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+
+    if (head->data == val)
+    {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+    Node *temp = head;
+    Node *prev = NULL;
+
+    while (temp)
+    {
+        if (temp->data == val)
+        {
+            prev->next = prev->next->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5, 6};
+    Node *head = convertArrayToLL(arr);
     printList(head);
-    cout<<endl;
-    cout<<"After Deleting kth"<<endl;
+    cout << endl;
+    cout << "After Deleting kth" << endl;
     head = deleteK(head, 2);
     printList(head);
 }
