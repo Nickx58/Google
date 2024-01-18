0641ad5f910a383e37da416ea1964f91b45c7eaf
Nickx58 <nikhilx58@gmail.com>
2024-01-18 20:29:52 +0530

    Basics Props

--- react/react-typescript/src/App.tsx
+++ react/react-typescript/src/App.tsx
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+import { Greet } from "./components/Greet";
+
+const bikesList = [{ name: "Speed 400" }, { name: "Scram 411" }];
+const carObject = {
+  name: "Virtus",
+  type: "Sedan",
+};
 
 function App() {
   return (
     <div className="App">
-      Header
+      <Greet bikesList={bikesList} name="Nikhil" carObject={carObject} />
     </div>
   );
 }
---
+++ react/react-typescript/src/components/Greet.tsx
@@ -0,0 +1,23 @@
+type GreetProps = {
+  name: string;
+  bikesList: {
+    name: string;
+  }[];
+  carObject: {
+    name: string;
+    type: string;
+  };
+};
+export const Greet = ({ name, bikesList, carObject }: GreetProps) => {
+  return (
+    <div>
+      <h2>Welcome {name}!. You can do it easily...</h2>
+      <h5>
+        {carObject.name} {carObject.type}
+      </h5>
+      {bikesList.map((bike) => {
+        return <h3 key={bike.name}>{bike.name}</h3>;
+      })}
+    </div>
+  );
+};
