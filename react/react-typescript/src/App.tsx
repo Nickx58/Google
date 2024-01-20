fbbc6a034152fe48d3ece64ef01ed1feee941add
Nickx58 <nikhilx58@gmail.com>
2024-01-20 23:32:57 +0530

    Style Type Props

--- react/react-typescript/src/App.tsx
+++ react/react-typescript/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Button } from "./components/Button";
+import { Container } from "./components/Container";
 import { Greet } from "./components/Greet";
 import { Heading } from "./components/Heading";
 import { Input } from "./components/Input";
@@ -22,6 +23,7 @@ function App() {
       </TextChildren>
       <Button handleClick={(e, n) => console.log("Called", e, n)} />
       <Input value="Nick" handleChange={(e) => console.log(e)} />
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
     </div>
   );
 }
