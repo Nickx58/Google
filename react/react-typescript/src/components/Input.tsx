0dc0a0cff3d7483fc689987766ca9311345f5d73
Nickx58 <nikhilx58@gmail.com>
2024-01-23 22:27:13 +0530

    useRef type

--- react/react-typescript/src/components/Input.tsx
+++ react/react-typescript/src/components/Input.tsx
@@ -1,7 +1,16 @@
+import { useEffect, useRef } from "react";
+
 type InputProps = {
   value: string;
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 export const Input = ({ handleChange, value }: InputProps) => {
-  return <input value={value} onChange={(e) => handleChange(e)} />;
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  });
+  return (
+    <input ref={inputRef} value={value} onChange={(e) => handleChange(e)} />
+  );
 };
