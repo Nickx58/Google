edadedadf42811cd75646492ce47d7f605df3747
Nickx58 <nikhilx58@gmail.com>
2024-01-26 13:03:41 +0530

    Func Types

--- react/react-typescript/src/typescript-refressher/first-app.ts
+++ react/react-typescript/src/typescript-refressher/first-app.ts
@@ -9,3 +9,15 @@ let userID: string | number = 'ac';
 let hobbies: Array<string>;
 //let hobbies: string[];
 hobbies = ["Cricket", "Riding"]
+
+// function type
+
+function add(a: number, b: number) : number {
+    return a+b;
+}
+
+function calculate(a:number, b:number, calc:(a:number,b:number) => number): void {
+    calc(a,b);
+}
+
+calculate(5,10,add);
