36eb8ee962f2584c9db76dae6f7e0c8e37ba212a
Nickx58 <nikhilx58@gmail.com>
2024-01-26 12:56:28 +0530

    Basic Types

---
+++ react/react-typescript/src/typescript-refressher/first-app.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// Types in TypeScript
+
+let userName: string;
+userName = "Nikhil";
+
+let userID: string | number = 'ac';
+
+let hobbies: Array<string>;
+//let hobbies: string[];
+hobbies = ["Cricket", "Riding"]
