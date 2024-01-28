c78af0dc32dcdf451ed1de53a0b67176287b91cd
Nickx58 <nikhilx58@gmail.com>
2024-01-28 12:59:39 +0530

    componentwithoutrefs

---
+++ react/react-ts-udemy/src/AdvanceComponents/Button.tsx
@@ -0,0 +1,18 @@
+import { ComponentPropsWithoutRef } from "react";
+
+type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+  href?: never;
+};
+type LinkProps = ComponentPropsWithoutRef<"a"> & {
+  href?: string;
+};
+
+function isLinkProps(props: ButtonProps | LinkProps): props is LinkProps {
+  return "href" in props;
+}
+export default function Button(props: ButtonProps | LinkProps) {
+  if (isLinkProps(props)) {
+    return <a className="button" {...props}></a>;
+  }
+  return <button className="button" {...props}></button>;
+}
--- react/react-ts-udemy/src/App.tsx
+++ react/react-ts-udemy/src/App.tsx
@@ -1,10 +1,19 @@
 import Input from "./AdvanceComponents/Input";
+import Button from "./AdvanceComponents/Button";
 
 export default function App() {
   return (
     <main>
       <Input id="name" label="Your Name" type="text" />
       <Input id="name" label="Your Name" type="number" />
+      <p>
+        <Button type="button">Button</Button>
+      </p>
+      <p>
+        <Button type="link" href="https://www.google.com">
+          Link
+        </Button>
+      </p>
     </main>
   );
 }
