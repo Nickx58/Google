a1d7d645a1be83e23a06ed467ac4bfd5c1a99a35
Nickx58 <nikhilx58@gmail.com>
2024-01-28 23:48:29 +0530

    Data Fetching Types

--- react/react-ts-udemy/src/App.tsx
+++ react/react-ts-udemy/src/App.tsx
@@ -1,10 +1,43 @@
 import Input from "./AdvanceComponents/Input";
 import Button from "./AdvanceComponents/Button";
 import Container from "./AdvanceComponents/Container";
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
+import BlogPosts, { BlogPost } from "./components/BlogPosts";
+import { get } from "./util/http";
+
+type DataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
 
 export default function App() {
   const ref = useRef<HTMLInputElement>(null);
+  const [posts, setPosts] = useState<BlogPost[]>([]);
+  const [error, setError] = useState<string>();
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const data = (await get(
+          "https://jsonplaceholder.typicode.com/posts"
+        )) as DataBlogPost[];
+        const blogPosts: BlogPost[] = data.map((post) => {
+          return {
+            id: post.id,
+            title: post.title,
+            text: post.body,
+          };
+        });
+        setPosts(blogPosts);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      }
+    }
+    fetchPosts();
+  }, []);
   return (
     <main>
       <Input id="name" label="Your Name" type="text" ref={ref} />
@@ -18,6 +51,7 @@ export default function App() {
         </Button>
       </p>
       <Container as={Button}>Click me</Container>
+      {error ? <p>Failed to fetch</p> : <BlogPosts posts={posts} />}
     </main>
   );
 }
