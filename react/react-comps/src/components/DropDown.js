26a0957efb06feb1bd752a53e14c3684d6afb373
Nickx58 <nikhilx58@gmail.com>
2024-02-02 00:11:34 +0530

    DropDown Markup

--- react/react-comps/src/components/DropDown.js
+++ react/react-comps/src/components/DropDown.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DropDown = ({ options }) => {
-  return <h1>Hello DropDOwn</h1>;
+  const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    console.log(option);
+  };
+  const renderOptions = options.map((option) => {
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
+  });
+  return (
+    <div>
+      <div onClick={handleClick}>Select</div>
+      {isOpen && <div>{renderOptions}</div>}
+    </div>
+  );
 };
 
 export default DropDown;
