0465c58ad92a0ff58e2f38a37603c124ec482f70
Nickx58 <nikhilx58@gmail.com>
2024-02-02 23:55:02 +0530

    CSS Changes

--- react/react-comps/src/components/DropDown.js
+++ react/react-comps/src/components/DropDown.js
@@ -12,16 +12,29 @@ const DropDown = ({ options, value, onChange }) => {
   };
   const renderOptions = options.map((option) => {
     return (
-      <div onClick={() => handleOptionClick(option)} key={option.value}>
+      <div
+        className="hover: bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
         {option.label}
       </div>
     );
   });
 
   return (
-    <div>
-      <div onClick={handleClick}>{value?.label || "Select"}</div>
-      {isOpen && <div>{renderOptions}</div>}
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleClick}
+      >
+        {value?.label || "Select"}
+      </div>
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderOptions}
+        </div>
+      )}
     </div>
   );
 };
