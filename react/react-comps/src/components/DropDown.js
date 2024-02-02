a3c3150acc01fb83f776b492467b92489221b381
Nickx58 <nikhilx58@gmail.com>
2024-02-03 00:35:46 +0530

    Panel Component

--- react/react-comps/src/components/DropDown.js
+++ react/react-comps/src/components/DropDown.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from "./Panel";
 
 const DropDown = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,7 +15,7 @@ const DropDown = ({ options, value, onChange }) => {
   const renderOptions = options.map((option) => {
     return (
       <div
-        className="hover: bg-sky-100 rounded cursor-pointer p-1"
+        className="hover: rounded cursor-pointer p-1"
         onClick={() => handleOptionClick(option)}
         key={option.value}
       >
@@ -24,17 +26,14 @@ const DropDown = ({ options, value, onChange }) => {
 
   return (
     <div className="w-48 relative">
-      <div
-        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
         onClick={handleClick}
       >
         {value?.label || "Select"}
-      </div>
-      {isOpen && (
-        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
-          {renderOptions}
-        </div>
-      )}
+        <GoChevronDown />
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderOptions}</Panel>}
     </div>
   );
 };
