1df0c09ed3d8a08321eedfee08064dc8825d41ce
Nickx58 <nikhilx58@gmail.com>
2024-02-04 17:18:19 +0530

    Route Component

--- react/react-comps/src/App.js
+++ react/react-comps/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordianPage from "./pages/AccordianPage";
+import DropDownPage from "./pages/DropdownPage";
 
 function App() {
   return (
     <div className="app">
-      <Link to="/accordion">Go to Accordion</Link>
+      <Link to="/accordian">Go to Accordian</Link>
       <Link to="/dropdown">Go to DropDown</Link>
+      <div>
+        <Route path="/accordian">
+          <AccordianPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropDownPage />
+        </Route>
+      </div>
     </div>
   );
 }
