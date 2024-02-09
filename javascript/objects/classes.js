8c5fa5d19e259b895bd35cb7e07722e13f119b7f
Nickx58 <nikhilx58@gmail.com>
2024-02-09 23:39:40 +0530

    Objects

---
+++ javascript/objects/classes.js
@@ -0,0 +1,13 @@
+class Math {
+  constructor(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+
+  add() {
+    return this.num1 + this.num2;
+  }
+}
+
+const add2 = new Math(2, 2);
+add2.add();
