96f317c910618ef6a39b654f86a33cd3856e3678
Nickx58 <nikhilx58@gmail.com>
2024-02-09 23:55:34 +0530

    Classes

--- javascript/objects/classes.js
+++ javascript/objects/classes.js
@@ -11,3 +11,28 @@ class Math {
 
 const add2 = new Math(2, 2);
 add2.add();
+
+class BankAccount {
+  constructor(accNum, accName, balance = 0) {
+    this.accNum = accNum;
+    this.accName = accName;
+    this.balance = balance;
+  }
+
+  deposit(amt) {
+    if (amt > 0) {
+      this.balance += amt;
+      console.log(`New Balance ${this.balance}`);
+    } else {
+      console.log("Cannot deposit the negative value");
+    }
+  }
+  withdraw(amt) {
+    if (amt > this.balance) {
+      console.log("You don't have enough funds to withdraw");
+    } else {
+      this.balance -= amt;
+      console.log(`New Balance ${this.balance}`);
+    }
+  }
+}
---
+++ javascript/objects/objects.md
@@ -0,0 +1,5 @@
+# Objects
+
+Finish the Object playlist from the udemy course.
+Read the javascript.info object session, MDN and you don't know JS.
+Finish everything that relates to objects like basics of object, how to iterate over objects, copy the object, Classes, this keyword. Basically finish every information out there on objects. So that you don't have to study again this topic ever.
