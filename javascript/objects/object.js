0a2393a61c0c13bd2d628d6caed4de5dd9ebe8d7
Nickx58 <nikhilx58@gmail.com>
2024-02-14 18:29:12 +0530

    DeepClone

--- javascript/objects/object.js
+++ javascript/objects/object.js
@@ -12,3 +12,24 @@ for(let key in user) {
     console.log(key);
     console.log(user[key])
 }
+
+/*
+One of the fundamental differences of objects versus primitives is that objects are stored and copied “by reference”,
+whereas primitive values: strings, numbers, booleans, etc – are always copied “as a whole value”.
+*/
+{
+
+let user = { name: "John" };
+
+let permissions1 = { canView: true };
+let permissions2 = { canEdit: true };
+
+// copies all properties from permissions1 and permissions2 into user
+Object.assign(user, permissions1, permissions2);
+
+// now user = { name: "John", canView: true, canEdit: true }
+
+// The call structuredClone(object) clones the object with all nested properties.
+
+let clone = structuredClone(user);
+}
