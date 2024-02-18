13e5e97fb3309eeb8689e220517d1086720a531a
Nickx58 <nikhilx58@gmail.com>
2024-02-18 23:30:12 +0530

    Promise ALL example

--- javascript/Async/Promise.js
+++ javascript/Async/Promise.js
@@ -21,3 +21,31 @@ fetch(URL).then(function (data) {
         console.log(err)
     })
 }
+
+// Promise ALL
+{
+
+    const p1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('P1 success')
+        }, 1000)
+    })
+
+    const p2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("P2 success")
+        }, 2000)
+    })
+
+    const p3 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("P3 success")
+        }, 3000)
+    })
+
+    Promise.all([p1, p2, p3]).then((data) => {
+        console.log(data);
+    }).catch(err => {
+        console.log(err)
+    })
+}
