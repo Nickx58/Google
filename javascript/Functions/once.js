14595817ccfdab3b895c765213ddd10bcb87da9f
Nickx58 <nikhilx58@gmail.com>
2024-02-22 17:43:32 +0530

    Once Function

---
+++ javascript/Functions/once.js
@@ -0,0 +1,13 @@
+function once(func) {
+    let called = false;
+
+    let result;
+
+    return function (...args) {
+        if (!called) {
+            result = func.apply(this, args);
+            called = true;
+        }
+        return result;
+    }
+}
