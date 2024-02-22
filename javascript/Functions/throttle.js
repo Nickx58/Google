0c82fb626647a709e2b39797bfa2dbf770657250
Nickx58 <nikhilx58@gmail.com>
2024-02-22 17:41:08 +0530

    Throttle

---
+++ javascript/Functions/throttle.js
@@ -0,0 +1,13 @@
+function throllte(func, limit) {
+    let flag = true;
+    return function (...args) {
+        let context = this;
+        if (flag) {
+            func.apply(context, args);
+            flag = false;
+            setTimeout(() => {
+                flag = true;
+            }, limit)
+        }
+    }
+}
