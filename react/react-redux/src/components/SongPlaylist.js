8ff5b66dd6f1ed013c13780cce60f107ba2ea793
Nickx58 <nikhilx58@gmail.com>
2024-03-08 00:15:15 +0530

    Updating state

--- react/react-redux/src/components/SongPlaylist.js
+++ react/react-redux/src/components/SongPlaylist.js
@@ -1,17 +1,25 @@
+import { useDispatch, useSelector } from "react-redux"
 import { createRandomSong } from "../data";
+import { addSong, removeSong } from "../store";
 
 function SongPlaylist() {
+    const dispatch = useDispatch();
     // To Do:
     // Get list of songs
-    const songPlaylist = [];
+    const songPlaylist = useSelector((state) => state.songs)
 
     const handleSongAdd = (song) => {
         // To Do:
         // Add song to list of songs
+        const action = addSong(song);
+        dispatch(action)
+
     };
     const handleSongRemove = (song) => {
         // To Do:
         // Remove song from list of songs
+        const action = removeSong(song);
+        dispatch(action)
     };
 
     const renderedSongs = songPlaylist.map((song) => {
--- react/react-redux/src/store/index.js
+++ react/react-redux/src/store/index.js
@@ -5,7 +5,12 @@ const songSlice = createSlice({
     initialState: [],
     reducers: {
         addSong(state, action) {
+            console.log(state)
             state.push(action.payload);
+        },
+        removeSong(state, action) {
+            const index = state.indexOf(action.payload);
+            state.splice(index, 1);
         }
     }
 })
@@ -17,3 +22,4 @@ const store = configureStore({
 })
 
 export { store };
+export const { addSong, removeSong } = songSlice.actions;
