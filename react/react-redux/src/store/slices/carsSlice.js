590cf0d22d0685d7981f1d4795c77b59ace24a2d
Nickx58 <nikhilx58@gmail.com>
2024-03-08 14:24:20 +0530

    Car Project slices

---
+++ react/react-redux/src/store/slices/carsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit"
+
+const carSlice = createSlice({
+    name: 'cars',
+    initialState: {
+        searchTerm: "",
+        cars: []
+    },
+    reducers: {
+        changeSearchTerm(state, action) {
+            state.searchTerm = action.payload
+        },
+        addCar(state, action) {
+            state.cars.push({
+                name: action.payload.name,
+                cost: action.payload.cost,
+                id: nanoid()
+            })
+        },
+        removeCar(state, action) {
+            const updated = state.cars.filter(car => car.id !== action.payload.id)
+            state.cars = updated
+        }
+    }
+})
+
+export const { searchTerm, addCar, removeCar } = carSlice.actions;
+export const carReducer = carSlice.reducer
