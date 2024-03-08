9adcb34238c04520de7b81e50cddebb434275201
Nickx58 <nikhilx58@gmail.com>
2024-03-08 15:25:52 +0530

    Car Form state

--- react/react-redux/src/components/CarForm.js
+++ react/react-redux/src/components/CarForm.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { changeName, changeCost, addCar } from '../store/index'
 
 const CarForm = () => {
+    const dispatch = useDispatch();
+    const name = useSelector(state => state.form.name)
+    const cost = useSelector(state => state.form.cost)
+
+    const handleNameChange = e => {
+        const value = e.target.value
+        dispatch(changeName(value))
+    }
+    const handleCostChange = e => {
+        const value = parseInt(e.target.value) || 0;
+        dispatch(changeCost(value))
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const carObject = {
+            name,
+            cost
+        }
+        dispatch(addCar(carObject))
+    }
     return (
-        <div>
-            CarForm
+        <div className="car-form panel">
+            <h4 className="subtitle is-3">Add Car</h4>
+            <form onSubmit={handleSubmit}>
+                <div className="field-group">
+                    <div className="field">
+                        <label className="label">Name</label>
+                        <input className="input is-expanded" type="text" value={name} onChange={handleNameChange} />
+                    </div>
+                    <div className="field">
+                        <label className="label">Cost</label>
+                        <input className="input is-expanded" type="number" value={cost || ''} onChange={handleCostChange} />
+                    </div>
+                </div>
+                <div className="field">
+                    <button className="button is-link">Submit</button>
+                </div>
+            </form>
         </div>
     )
 }
--- react/react-redux/src/store/index.js
+++ react/react-redux/src/store/index.js
@@ -1,14 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { songReducer, addSong, removeSong } from "./slices/songSlice";
 import { movieReducer, addMovie, removeMovie } from "./slices/movieSlice";
+import { carReducer, searchTerm, addCar, removeCar } from './slices/carsSlice'
+import { formReducer, changeName, changeCost } from './slices/formSlice'
 import { reset } from "./actions";
 
 
 const store = configureStore({
     reducer: {
         songs: songReducer,
-        movies: movieReducer
+        movies: movieReducer,
+        form: formReducer,
+        cars: carReducer
     }
 })
 
-export { store, reset, addMovie, removeMovie, addSong, removeSong };
+export { store, reset, addMovie, removeMovie, addSong, removeSong, searchTerm, addCar, removeCar, changeCost, changeName };
