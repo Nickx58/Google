5e7dc9e9709acb309f81f5f0e34d505f06a51570
Nickx58 <nikhilx58@gmail.com>
2024-03-09 19:59:59 +0530

    Array Object Javascript question

--- Uber/Javascript/uniqueArray.js
+++ Uber/Javascript/uniqueArray.js
@@ -42,3 +42,60 @@ console.log(getUnique(cars)); // ["santro", "i20", "maruti", "virtus", "polo"]
         return unique;
     }
 }
+
+{
+    /*
+[{
+  count: 2,
+  victim: "React"
+}, {
+  count: 1,
+  victim: "Angular"
+}, {
+  count: 2,
+  victim: "Php"
+}, {
+  count: 1,
+  victim: "Vue"
+}]
+    */
+    const arr = [
+        {
+            tech: 'React', vulnerability: true
+        }, {
+            tech: 'Angular', vulnerability: true
+        },
+        {
+            tech: 'React', vulnerability: false
+        },
+        {
+            tech: 'Php', vulnerability: false
+        },
+        {
+            tech: 'Vue', vulnerability: true
+        },
+        {
+            tech: 'Php', vulnerability: false
+        }
+    ]
+
+    function returnCount(arr) {
+        const obj = {};
+        arr.forEach((el) => {
+            const key = el.tech;
+            if (el.tech) {
+                obj[key] = (obj[key] || 0) + 1;
+            } else {
+                obj[key] = (obj[key] || 0) + 1;
+            }
+        })
+        const n = [];
+        Object.keys(obj).map((item) => {
+            n.push({ victim: item, count: obj[item] });
+        })
+        return n;
+    }
+
+    console.log(returnCount(arr));
+
+}
