b26c4f7497f40e8e9af44dda9d0f5d4693d90e1d
Nickx58 <nikhilx58@gmail.com>
2024-03-11 09:26:21 +0530

    Object question

--- Uber/Javascript/objectOutput.js
+++ Uber/Javascript/objectOutput.js
@@ -20,21 +20,64 @@ Kia: {
  */
 
 const myCars = {
-    Nissan: 'Magnite',
-    Tata: 'Nexon',
-    Kia: 'Sonet'
+  Nissan: 'Magnite',
+  Tata: 'Nexon',
+  Kia: 'Sonet'
 }
 
 function getCar(obj) {
-    const newObj = {};
+  const newObj = {};
 
-    Object.keys(obj).forEach((car) => {
-        newObj[car] = {
-            name: obj[car],
-            type: 'car'
-        }
-    })
-    return newObj;
+  Object.keys(obj).forEach((car) => {
+    newObj[car] = {
+      name: obj[car],
+      type: 'car'
+    }
+  })
+  return newObj;
 }
 
 console.log(getCar(myCars));
+
+{
+  const arr = [
+    { id: 1, role: "Developer" },
+    { id: 2, role: "Scrum Master" },
+    { id: 3, role: "Delivery Manager" },
+    { id: 1, role: "Team Leader" },
+    { id: 2, role: "Project Coordinator" },
+  ];
+
+  function merge(arr) {
+    let obj = {};
+    // group role by id
+    arr.forEach((item) => {
+      if (obj[item.id]) {
+        obj[item.id] = [item.role, ...obj[item.id]]
+      } else {
+        obj[item.id] = [item.role];
+      }
+
+    })
+    let roles = [];
+    // convert obj to array of objects
+    Object.keys(obj).forEach((item) => {
+      roles.push({ id: item, role: obj[item] })
+    })
+    return roles;
+  }
+  console.log(merge(arr));
+  /*
+  output
+  [{
+  id: "1",
+  role: ["Team Leader", "Developer"]
+}, {
+  id: "2",
+  role: ["Project Coordinator", "Scrum Master"]
+}, {
+  id: "3",
+  role: ["Delivery Manager"]
+}]
+  */
+}
---
+++ Uber/todo.md
@@ -0,0 +1,32 @@
+use prototype inheritance to create a polyfill for reduce function
+implement flattening of array. [1, 2, 3, 4, [5, 6, 7, 8, [9, 10]]] -> [1,2,3,4,5,6,7,8, 9, 10]
+Create select dropdown component using plain HTML and CSS. ( not to use input and select tag)
+React optimization
+Event Loop and Queues
+
+difference between Prototype and __proto__
+ console.log(null+undefined);
+console.log(null-3);
+console.log(5+undefined);
+if(null==undefined)
+
+Uber system design interview
+Design Uber Bus and describe the various services required for it. 
+What is your favourite website/app with respect to UI/UX and what can you do to improve it?
+How to design a server side rendering system without using framework (like NextJS)?
+How to handle deadlines in case team members go on unplanned leaves.
+
+console.log(null + undefined)
+console.log(true - 3)
+console.log(typeof typeof [2,3,4])
+function sum (p = 3, p) { alert(p) }
+sum(2)
+What are the new features of ES6?
+Questions related to this and call, bind apply and context of this in arrow functions
+Closures and when to use them
+Write a function to convert number to text 
+convert(546) // Output -> Five Hundred Sixty Four
+React hooks questions useCallback, useMemo, React.memo etc
+React problem to dynamically change a list style based on user input from a dropdown
+Asked about basic knowledge of Docker, Jenkins CI/CD
+Asked about PR reviews and experience with JIRA
