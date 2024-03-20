6010687ed524bf0f2d8f268fb35a5ef4c60ee499
Nickx58 <nikhilx58@gmail.com>
2024-03-20 23:43:07 +0530

    Subarray with zero sum

---
+++ DS/HashMap/2.SubArrayWithZeroSum.cpp
@@ -0,0 +1,18 @@
+bool subArrayExists(int arr[], int n)
+{
+    // Your code here
+    unordered_set<int> sum = {0};
+    int currentSum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        currentSum += arr[i];
+        if (sum.find(currentSum) != sum.end())
+        {
+            return true;
+        }
+        sum.insert(currentSum);
+    }
+    return false;
+}
+}
+;
