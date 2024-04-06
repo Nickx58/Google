0324bbfd18b0c525e8635fad2e14ec8a16870b4a
Nickx58 <nikhilx58@gmail.com>
2024-04-07 00:00:58 +0530

    CodeChef: 513 Rating Problem

---
+++ CP/CodeChef/500-100/2.Assignments.cpp
@@ -0,0 +1,26 @@
+// Janmansh and Assignments
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/JASSIGNMENTS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int time;
+        cin >> time;
+        if (time + 3 <= 10)
+        {
+            cout << "YES";
+        }
+        else
+        {
+            cout << "NO";
+        }
+        cout << endl;
+    }
+}
