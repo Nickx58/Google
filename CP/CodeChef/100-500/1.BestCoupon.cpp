7285c158580fd12ae72ebb2c1d68280a2156885c
Nickx58 <nikhilx58@gmail.com>
2024-04-18 12:49:40 +0530

    CodeCheff: 496

---
+++ CP/CodeChef/100-500/1.BestCoupon.cpp
@@ -0,0 +1,26 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHEAPFOOD
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+
+        if (x >= 1000)
+        {
+            cout << x / 10 << endl;
+        }
+        else
+        {
+            cout << 100 << endl;
+        }
+    }
+}
