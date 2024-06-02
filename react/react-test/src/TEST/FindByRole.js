aed64da5dc78e8427b2f54fa194bf1bdcb07360e
Nickx58 <nikhilx58@gmail.com>
2024-06-02 14:50:05 +0530

    Test: Find By Role

---
+++ react/react-test/src/TEST/FindByRole.js
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react';
+
+function RoleExample() {
+  return (
+    <div>
+      <a href="/">Link</a>
+      <button>Button</button>
+      <footer>Footer Content</footer>
+      <h1>Heading</h1>
+      <header>Banner</header>
+      <img alt="description" /> img
+      <input type="checkbox" /> checkbox
+      <input type="number" /> Number
+      <input type="radio" /> radio
+      <input type="text" /> Text
+      <li> ListItem</li>
+      <ul>List Group </ul>
+    </div>
+  );
+}
+
+test('can find element by role', () => {
+  render(<RoleExample />);
+
+  const roles = [
+    'link',
+    'button',
+    'contentinfo',
+    'heading',
+    'banner',
+    'img',
+    'checkbox',
+    'spinbutton',
+    'radio',
+    'textbox',
+    'listitem',
+    'list',
+  ];
+  for (let role of roles) {
+    const el = screen.getByRole(role);
+    expect(el).toBeInTheDocument();
+  }
+});
