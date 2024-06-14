bbded9a93070d5ad3cefcf62e714ba70df7d5dc1
Nickx58 <nikhilx58@gmail.com>
2024-06-14 12:14:30 +0530

    React: Test Query Functions

---
+++ react/react-test/src/TEST/QueryFunction.js
@@ -0,0 +1,60 @@
+import { render, screen } from '@testing-library/react';
+
+function ColorList() {
+    return (
+        <ul>
+            <li>Black</li>
+            <li>Blue</li>
+            <li>Green</li>
+        </ul>
+    );
+}
+
+test('getBy, queryBy and findBy when we find 0 element', async () => {
+    render(<ColorList />);
+    expect(
+        () => screen.getByRole('textbox')
+    ).toThrow();
+
+    expect(screen.queryByRole('textbox')).toEqual(null);
+
+    let errorThrow = false;
+    try {
+        await screen.findByRole('textbox');
+    } catch (err) {
+        errorThrow = true;
+    }
+    expect(errorThrow).toEqual(true);
+});
+
+test('getBy, queryBy and findBy when we find 1 element', async () => {
+    render(<ColorList />);
+    expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(screen.queryByRole('list')).toBeInTheDocument();
+    expect(await screen.findByRole('list')).toBeInTheDocument();
+});
+
+test('getBy, queryBy and findBy when we find > 1 element', async () => {
+    render(<ColorList />);
+    expect(
+        () => screen.getByRole('listitem')
+    ).toThrow();
+
+    expect(() => screen.queryByRole('listitem')).toThrow();
+
+    let errorThrow = false;
+    try {
+        await screen.findByRole('listitem');
+    } catch (err) {
+        errorThrow = true;
+    }
+    expect(errorThrow).toEqual(true);
+});
+
+test('getAllBy, queryAllBy and findAllBy', async () => {
+    render(<ColorList />);
+
+    expect(screen.getAllByRole('listitem')).toHaveLength(3);
+    expect(screen.queryAllByRole('listitem')).toHaveLength(3);
+    expect(await screen.findAllByRole('listitem')).toHaveLength(3);
+});
