a7b8b5dab1cbafb15c447d70286bb6d50eecc394
Nickx58 <nikhilx58@gmail.com>
2024-06-29 17:50:17 +0530

    GreatFrontEnd: Sleep

---
+++ Atlassian/GreatFrontEnd/3.Sleep.js
@@ -0,0 +1,13 @@
+// In JavaScript, the setTimeout function can be used to execute callbacks after a delay.
+// However, it introduces a layer of nesting in the code which may not be desired.
+// It'd be nice to use setTimeout in this fashion:
+
+/**
+ * @param {number} duration
+ * @return {Promise<void>}
+ */
+export default async function sleep(duration) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, duration);
+    });
+}
