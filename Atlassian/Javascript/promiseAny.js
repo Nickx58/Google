28a25ee5dd1fb77663df57c8d72db59707edadcd
Nickx58 <nikhilx58@gmail.com>
2024-07-23 20:23:14 +0530

    Promise Any

---
+++ Atlassian/Javascript/promiseAny.js
@@ -0,0 +1,38 @@
+const p0 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(42);
+  }, 100);
+});
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("Err!");
+  }, 400);
+});
+
+export default function promiseAny(iterable) {
+  return new Promise((resolve, reject) => {
+    if (iterable.length === 0) {
+      reject(new AggregateError([]));
+    }
+
+    let pending = iterable.length;
+
+    const errors = [];
+
+    iterable.forEach(async (item, index) => {
+      try {
+        const value = await item;
+        resolve(value);
+      } catch (err) {
+        errors[index] = err;
+        pending--;
+
+        if (pending === 0) {
+          reject(new AggregateError(errors));
+        }
+      }
+    });
+  });
+}
+
+await promiseAny([p0, p1]); // 42
