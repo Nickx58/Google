3d6c19bda2e1556dd97d9f13539579c654bde7ba
Nickx58 <nikhilx58@gmail.com>
2024-08-07 20:26:29 +0530

    Move Zero Front Without changing order

--- DS/Array/7.MoveZeroFront.cpp
+++ DS/Array/7.MoveZeroFront.cpp
@@ -1,35 +1,65 @@
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int main()
+// {
+//     // Write C++ code here
+//     vector<int> water = {0, 1, 2, 3, 0, 0, 0, 1, 0};
+
+//     int start = 0;
+//     int end = water.size() - 1;
+//     while (start < end)
+//     {
+//         if (water[start] == 0)
+//         {
+//             start++;
+//         }
+//         else if (water[end] != 0)
+//         {
+//             end--;
+//         }
+//         else
+//         {
+//             swap(water[start], water[end]);
+//             start++;
+//             end--;
+//         }
+//     }
+//     for (auto it : water)
+//     {
+//         cout << it << " ";
+//     }
+//     return 0;
+// }
+
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-int main()
-{
-    // Write C++ code here
-    vector<int> water = {0, 1, 2, 3, 0, 0, 0, 1, 0};
+int main() {
+    vector<int> move = {1, 2, 0, 0, 4, 0, 1, 0};
+    int n = move.size();
+    int nonZeroIndex = n - 1;  // Start from the end
 
-    int start = 0;
-    int end = water.size() - 1;
-    while (start < end)
-    {
-        if (water[start] == 0)
-        {
-            start++;
-        }
-        else if (water[end] != 0)
-        {
-            end--;
-        }
-        else
-        {
-            swap(water[start], water[end]);
-            start++;
-            end--;
+    // Move non-zero elements to the end
+    for (int i = n - 1; i >= 0; --i) {
+        if (move[i] != 0) {
+            move[nonZeroIndex] = move[i];
+          nonZeroIndex--;
         }
     }
-    for (auto it : water)
-    {
-        cout << it << " ";
+
+    // Fill the rest with zeroes
+    for (int i = nonZeroIndex; i >= 0; --i) {
+        move[i] = 0;
     }
+
+    // Print the result
+    for (int it : move) {
+        cout << it << ' ';
+    }
+
     return 0;
 }
